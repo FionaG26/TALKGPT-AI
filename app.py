468ed54ae1db452fd8cec72e68dab94939675298
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2023-06-11 10:46:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import openai
 app = Flask(__name__)
 
 # Set your OpenAI API key
-openai.api_key =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
+openai.api_key = "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"
 
 # Define a route for the chat endpoint
 @app.route("/chat", methods=["POST"])
@@ -14,23 +14,23 @@ def chat():
 
     # Send the user's message to the ChatGPT model
     response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=user_message,
-            max_tokens=50,
-            temperature=0.8,
-            n=1,
-            stop=None,
-            temperature=0.8,
-            top_p=None,
-            frequency_penalty=None,
-            presence_penalty=None,
-     )
+        engine="text-davinci-003",
+        prompt=user_message,
+        max_tokens=50,
+        temperature=0.8,
+        n=1,
+        stop=None,
+        temperature=0.8,
+        top_p=None,
+        frequency_penalty=None,
+        presence_penalty=None
+    )
 
-        # Get the generated response from the model
-        chat_response = response.choices[0].text.strip()
+    # Get the generated response from the model
+    chat_response = response.choices[0].text.strip()
 
-        # Return the response to the frontend
-        return jsonify({"message": chat_response})
+    # Return the response to the frontend
+    return jsonify({"message": chat_response})
 
 if __name__ == "__main__":
-        app.run()
+    app.run()
